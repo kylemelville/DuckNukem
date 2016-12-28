7c016f7967ab8b640711fa972190557b79c751ed
Kyle Melville <KPMelville@gmail.com>
2016-12-28 14:24:47 -0600

    Adds core files and pistol.

---
+++ src/DNPistol.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using DuckGame;
+
+namespace DuckNukem.src
+{
+    [EditorGroup("Duck Nukem")]
+    public class DNPistol : Gun
+    {
+        SpriteMap _sprite;
+
+        public DNPistol(float xval, float yval) : base(xval, yval)
+        {
+            _ammoType = new AT9mm();
+            _barrelOffsetTL = new Vec2(17f, 3f);
+            _bio = "You should probably find something better.";
+            _editorName = "Pistol";
+            _fireSound = GetPath("sounds/pistol.wav");
+            _fullAuto = false;
+            _fireWait = 1f;
+            _sprite = new SpriteMap(GetPath("sprites/PistolSprite.png"), 16, 10, false);            
+            _sprite.AddAnimation("empty", 0.1f, false, new int[] { 1 });
+            ammo = 12;
+            center = new Vec2(10f, 4f);
+            collisionOffset = new Vec2(-9f, -6f);
+            collisionSize = new Vec2(16f, 11f);
+            graphic = _sprite;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void OnPressAction()
+        {
+            base.OnPressAction();
+            if(this.ammo > 0)
+            {
+                this._sprite.AddAnimation(("gfire" + ammo ), 0.1f, false, new int[] { 1, 0 });
+                this._sprite.SetAnimation("gfire" + ammo);
+            }
+            else
+            {
+                this._sprite.SetAnimation("empty");
+            }            
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
---
+++ src/DNShotgun.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DuckGame;
+
+namespace DuckNukem.src
+{
+    [EditorGroup("Duck Nukem")]
+    class DNShotgun : Shotgun
+    {
+        public DNShotgun(float xval, float yval) : base(xval, yval)
+        {
+            
+            _ammoType = new ATShotgun();
+            _barrelOffsetTL = new Vec2(34f, 3f); 
+            _bio = "Packs a far more powerful blast than the pistol.";
+            _editorName = "Shotgun";
+            _fireSound = GetPath("sounds/shotgun.wav");
+            _fullAuto = false;
+            _fireWait = 2f;
+            //_kickForce = 3.5f;
+            _numBulletsPerFire = 5;
+            ammo = 20;
+            center = new Vec2(14f, 5f);
+            collisionOffset = new Vec2(-12f, -3f);
+            collisionSize = new Vec2(32f, 9f);
+            graphic = new SpriteMap(GetPath("sprites/ShotgunSprite.png"), 28, 12, false);
+            loseAccuracy = 0.3f;
+            maxAccuracyLost = 0.9f;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void OnPressAction()
+        {
+            base.OnPressAction();
+            //if (this.ammo > 0)
+            //{
+            //    this._sprite.AddAnimation(("gfire" + ammo), 0.1f, false, new int[] { 1, 0 });
+            //    this._sprite.SetAnimation("gfire" + ammo);
+            //}
+            //else
+            //{
+            //    this._sprite.SetAnimation("empty");
+            //}
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
